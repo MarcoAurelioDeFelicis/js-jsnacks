1e5d06a985b800923469652ad06aca6970c39ca6
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-17 18:27:32 +0100

    ADD: creazione index.htm e script.js di exercize 4 + array nomi lista del party e prompt per chiedere il nome dell'utente

---
+++ exercize4/js4/script.js
@@ -0,0 +1,9 @@
+console.log ('snacks');
+
+// creare un array con i nomi in list per il party
+const arraynames = [Marco, Vaiva, Mick, Nastya, Fabio, Riccardo, Austeya, Martina];
+
+// chiedere all'utente il proprio nome
+const username = prompt ('inserisci il tuo nome') //string or nun
+
+

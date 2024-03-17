c62a75351f0356f83034a0479a91a5e9a3bd6ba0
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-17 16:37:37 +0100

    ADD: richiesta 10 numeri in prompt separati all'utente

---
+++ exercize3/js3/script.js
@@ -0,0 +1,17 @@
+
+console.log ('snacks');
+
+//cheiedere all'utente 10 numeri
+
+const num1 = parseInt (prompt ('inserisci qui un numero'))
+const num2 = parseInt (prompt ('inserisci qui un altro numero'))
+const num3 = parseInt (prompt ('inserisci qui un altro numero'))
+const num4 = parseInt (prompt ('inserisci qui un altro numero'))
+const num5 = parseInt (prompt ('inserisci qui un altro numero'))
+const num6 = parseInt (prompt ('inserisci qui un altro numero'))
+const num7 = parseInt (prompt ('inserisci qui un altro numero'))
+const num8 = parseInt (prompt ('inserisci qui un altro numero'))
+const num9 = parseInt (prompt ('inserisci qui un altro numero'))
+const num10 = parseInt (prompt ('inserisci qui lultimo numero'))
+
+const result = (num1 + )

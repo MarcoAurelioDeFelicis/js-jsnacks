6b78333ddb4b24448cf300497f8b6784dd2ff4ca
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-17 14:51:18 +0100

    ADD: prompt num1 e num2 con consol log + if,else del numero maggiore

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log ('snacks')
+
+//chiedere all'ìutente due numeri in prompt separati
+const num1 = parseInt (prompt ('inserisci il tuo numero')) //number
+console.log (num1 + 'questo è il tuo primo numero')
+
+const num2 = parseInt (prompt('inserisci un altro numero')) //number
+console.log (num2 + 'questo è il tuo secondo numero')
+
+//stampare in console solo il numero maggiore digitati dall'utente
+if (num1 > num2) {
+    console.log (num1 + 'questo è il numero maggiore')
+    
+} else {
+    console.log (num2 + 'questpo è il numero maggiore')
+}

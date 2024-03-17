0aaaffab8d5b81c4c54a58745b9b143d1db592a5
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-17 14:55:48 +0100

    ADD: stampe in consol log con spazi tra numero e commento

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@ console.log ('snacks')
 
 //chiedere all'ìutente due numeri in prompt separati
 const num1 = parseInt (prompt ('inserisci il tuo numero')) //number
-console.log (num1 + 'questo è il tuo primo numero')
+console.log (num1 + ' questo è il tuo primo numero')
 
 const num2 = parseInt (prompt('inserisci un altro numero')) //number
-console.log (num2 + 'questo è il tuo secondo numero')
+console.log (num2 + ' questo è il tuo secondo numero')
 
 //stampare in console solo il numero maggiore digitati dall'utente
 if (num1 > num2) {
-    console.log (num1 + 'questo è il numero maggiore')
+    console.log ('questo è il numero maggiore ' + num1)
     
-} else {
-    console.log (num2 + 'questpo è il numero maggiore')
+} 
+else {
+    console.log ('questpo è il numero maggiore ' + num2)
 }

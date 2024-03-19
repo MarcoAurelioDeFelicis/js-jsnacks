47de64e5735d5ca66072b9eb63f4b9b0de13f6aa
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-19 19:25:56 +0100

    ADD: commenti

--- exercize4/js4/script copy.js
+++ exercize4/js4/script copy.js
@@ -6,19 +6,26 @@ const arraynames = [ "Marco", "Vaiva", "Mick", "Nastya", "Fabio","Gaya", "Riccar
 // chiedere all'utente il proprio nome
 const username = prompt ('inserisci il tuo nome') //string or nun
 
+//variabile true di nome inserito
 let trovato = false
 
+// ciclo for per controllare scorrendo l'array della lista invitati
 for (let i = 0; i < arraynames.length; i++) {
     const currentname = arraynames [i]
+    //se il nome nell'array e uguale al nome utente allora è true
     if (currentname === username) {
         trovato = true
     }
-    console.log (currentname, username, trovato)
+    // console.log (currentname, username, trovato)
 
 }
 
+//se l'utente è true 
 if (trovato) {
-    console.log ('puoi')
+    //allora può entrare
+    console.log ('puoi partecipare al party')
+    //altrimenti
 } else {
-    console.log ('non puoi')
+    //non può entrare
+    console.log ('non puoi partecipare al party')
 }

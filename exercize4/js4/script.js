b548e895e6777a375e1d6fc8e4497824c639ea37
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-19 15:49:24 +0100

    ADD: situazione di stalla sulla stampa in console dopo verifica del nome utente nell'array

--- exercize4/js4/script.js
+++ exercize4/js4/script.js
@@ -1,9 +1,27 @@
 console.log ('snacks');
 
 // creare un array con i nomi in list per il party
-const arraynames = [Marco, Vaiva, Mick, Nastya, Fabio, Riccardo, Austeya, Martina];
+const arraynames = [ "Marco", "Vaiva", "Mick", "Nastya", "Fabio","Gaya", "Riccardo", "Austeya", "Martyna" ];
 
 // chiedere all'utente il proprio nome
 const username = prompt ('inserisci il tuo nome') //string or nun
 
+// const invitato = arraynames  
+
+for (let i = 0; i < arraynames.length; i++) {
+
+    // const nomecorrente = arraynames [i]
+
+    if (username === arraynames [i]) {
+        console.log ('ciao ' + username + ' puoi entrare al paty')
+        
+    } else if (username != arraynames [i] ){
+        console.log ('ci dispiace ma il tuo nome non è sulla lista')
+        
+    }else {
+
+    }
+    
+
+}
 

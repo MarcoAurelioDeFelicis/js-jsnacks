2bdd3ba032f4214fc2874b79d50d6f061312f540
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-19 16:29:45 +0100

    ADD: js- stampa del numero random dopo usernumber

--- exercize6/js6/script.js
+++ exercize6/js6/script.js
@@ -2,11 +2,11 @@ console.log ('snacks');
 
 //Fate generare un numero random da 0 a 10 al computer, 
  let randomfloat = Math.floor(Math.random() *10) +1; //number 1 - 10
- console.log ('questo è il numero casuale ' + randomfloat);
 
 //e chiedete all'utente di inserire un suo numero.
 const usernumber = parseInt( prompt ('inserisci un numero da 1 a 10')); //number
 console.log ('questo è il tuo numero ' + usernumber);
+console.log ('questo è il numero casuale ' + randomfloat);
 
 //Se il numero scelto dall'utente è uguale al numero del computer 
 if (usernumber === randomfloat ) {
